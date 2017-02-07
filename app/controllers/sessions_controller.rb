c0919dc9676ff91b6786f7b61e42427a13293207
Ezekiel Hammond <ezekiel_hammond@dell.com>
2017-02-06 20:59:51 -0600

    Added Logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
 
 	def new
 	end
-	
+
 	def create
 	  @user = User.find_by_email(params[:session][:email])
 	  if @user && @user.authenticate(params[:session][:password])
@@ -12,5 +12,9 @@ class SessionsController < ApplicationController
 	  else
 	    redirect_to 'login'
 	  end 
+	end
+	def destroy 
+	  session[:user_id] = nil 
+	  redirect_to '/' 
 	end
 end
