69db566f5371245d577a594ca34222e2adbe5ad4
Ezekiel Hammond <ezekiel_hammond@dell.com>
2017-02-08 11:37:44 -0600

    building User Profile

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
   post 'login' => 'sessions#create'
 	
   delete 'logout' => 'sessions#destroy'
+
+  get 'users/show/:id' => 'users#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
