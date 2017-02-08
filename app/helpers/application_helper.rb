66cdb88d03010bf70352c81ee77e3c5466eeabee
Ezekiel Hammond <ezekiel_hammond@dell.com>
2017-02-08 13:16:35 -0600

    building User Profile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,6 @@ module ApplicationHelper
 	def gravatar_for(user)
 		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
 		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-		image_tag(gravatar_url, alt: user.firstname, class: "img-circle")
+		image_tag(gravatar_url, alt: user.first_name, class: "img-circle")
 	end
 end
