abab6a57882c1d2330fe52fedfe85c77671bfd79
Jafesu <Jafesu@outlook.com>
2017-02-08 15:23:17 -0600

    allowing images

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170208053708) do
+ActiveRecord::Schema.define(version: 20170208171405) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "post_id"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20170208053708) do
     t.string   "last_name",  null: false
     t.string   "email",      null: false
     t.text     "post",       null: false
+    t.integer  "user_id",    null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -38,6 +39,7 @@ ActiveRecord::Schema.define(version: 20170208053708) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin",           default: false
   end
 
 end
