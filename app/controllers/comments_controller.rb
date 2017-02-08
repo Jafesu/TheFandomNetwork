8eebbeb117176df93729906d1fcd0fc08344bcd7
Ezekiel Hammond <ezekiel_hammond@dell.com>
2017-02-08 10:44:41 -0600

    Testing for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,9 @@
 class CommentsController < ApplicationController
 	def create
- 
-	    Comment.create(:email, :first_name, :last_name, :post, :post_id)
-
-		redirect_to post
+		Comment.create(comment_params)
 	end
 
 	private
-		def comment_params
-			params.require(:comment).permit(:email, :first_name, :last_name, :post, :post_id)
-		end
-end
+
+	def comment_params
+		params.require(:comment).permit(:email, :first_name, :last_name, :post, :post_id)
