db59f967ffbde417d4b85f7829487ff9fd8d210e
Ezekiel Hammond <ezekiel_hammond@dell.com>
2017-02-08 18:11:18 -0600

    building User Profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+	has_attached_file :avatar, styles:{large: "600x600>", medium: "300x300>", thumb: "80x80#"}, default_url: "/images/images.jpg"
+	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
 	has_secure_password
 
